7b1f331888090d4e1f613f8762afcf695e5ac0c5
TauVip <Abylai1393@gmail.com>
2021-01-16 12:48:32 +0600

    first commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter } from 'react-router-dom'
+
+import './scss/app.scss'
+
+import App from './App'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById('root')
+)
+
+// #5: React Pizza — разработка интернет-магазина (for junior) | 1:49:20
---
+++ src/pages/Cart.jsx
@@ -0,0 +1,8 @@
+function Cart() {
+  return (
+    <div>
+      <h1>Корзина</h1>
+    </div>
+  )
+}
+export default Cart
